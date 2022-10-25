db5a3bff42e7a1cbe763a1d1510b8ac6637f13a9
rafly7 <39668448+rafly7@users.noreply.github.com>
2022-10-25 16:44:15 +0700

    Add unit testing in endpoint /api/v1/contributors (#30)
    * Change path to make cross environtment in windows, linux, and macOS
    
    * Add unit testing router in endpoint /api/v1/
    
    * Resolved conflict
    
    Resolved conflict with deleted package-lock.json
    
    * Add unit testing
    
    Co-authored-by: Ibnu syawal <46734417+ibnusyawall@users.noreply.github.com>

--- __test__/router.test.js
+++ __test__/router.test.js
@@ -6,13 +6,22 @@ beforeAll(() => {
 })
 
 describe('Route /api/v1', () => {
-  it("Should return a HTTP status code 200 with response Hello World", (done) => {
+  it("Should return a HTTP status code 200 in endpoint / with response Hello World", (done) => {
     request(server)
       .get('/api/v1/')
       .then(res => {
         expect(res.status).toEqual(200)
         expect(res.body).toMatchObject({message: 'Hello world!'})
         expect(res.body.message).toEqual('Hello world!')
+        done()
+      })
+  })
+  
+  it("Should return a HTTP status code 200 in endpoint /contributors", (done) => {
+    request(server)
+      .get('/contributors')
+      .then(res => {
+        expect(res.status).toEqual(200)
         done()
       })
   })
--- src/app.js
+++ src/app.js
@@ -1,63 +1,8 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-var cors = require('cors')
-var limitter = require('express-rate-limit')
+const server = require('./server')
 
-var webRouter = require('./routes/web')
-var apiRouter = require('./routes/api')
-
-var app = express()
-var PORT = process.env.PORT || 8000
-var server = app.listen(PORT)
-
-var limitRequest = limitter({
-  windowMs: 1 * 60 * 1000,
-  max: 20,
-  message: JSON.stringify({
-    message: 'to many request'
-  })
+const PORT = process.env.PORT || 8000
+server.listen(PORT, function() {
+  if (server.listening) {
+    console.log(`Server is listening on port ${PORT}`);
+  }
 })
-
-
-var DEBUG
-if (process.argv[2] == 'dev') {
-  DEBUG = true
-} else {
-  DEBUG = false
-}
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.set('json spaces', 2)
-
-app.use(cors({ origin: '*' }))
-
-if (!DEBUG) {
-  app.use(limitRequest)
-}
-
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/', webRouter)
-app.use('/api/v1', apiRouter)
-
-app.use(function(req, res, next) {
-    next(createError(404))
-})
-
-app.use(function(err, req, res, next) {
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-    res.status(err.status || 500)
-    res.render('error')
-})
-
-module.exports = app
